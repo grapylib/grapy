b4e8ce9465258c8e52bff8b4b602786d16ffdc11
Andres <grapylib@gmail.com>
2016-06-22 16:51:14 -0500

    Grapy Lib Version 1.0

--- grapy.py
+++ grapy.py
@@ -1,8 +1,10 @@
 import os,sys
-import pygame 
+import pygame
 
 ROJO=(255,0,0)
 BLANCO=(255,255,255)
+ANCHO=1000
+ALTO=600
 
 class Vertice(pygame.sprite.Sprite):
         
@@ -10,66 +12,104 @@ class Vertice(pygame.sprite.Sprite):
             pygame.sprite.Sprite.__init__(self)
             self.click = False
             self.sel=False
+            self.id=0
             self.img1=pygame.image.load('grapy/img/vminb.png').convert_alpha()
             self.img2=pygame.image.load('grapy/img/vminr.png').convert_alpha()
             self.image = self.img1
             self.rect = self.image.get_rect()
 
         def update(self,surface):
-            if self.sel:
-                if self.click:
+            if self.click:
                     self.image = self.img2
                     self.rect = self.image.get_rect() 
                     self.rect.center = pygame.mouse.get_pos()
-                else:
-                    self.image = self.img1
-                    #self.rect = self.image.get_rect()
-                if self.rect.x > 55:
-                   surface.blit(self.image,self.rect)
-                else:
-                    self.rect.x=55
-                    surface.blit(self.image,self.rect)
+            else:
+                self.image = self.img1
+                #self.rect = self.image.get_rect()
+            if self.rect.x > 55:
+                surface.blit(self.image,self.rect)
+            else:
+                self.rect.x=55
+                surface.blit(self.image,self.rect)
                 
 
 def seleccion(pantalla):
     vb=pygame.image.load('grapy/img/vb.png').convert_alpha()    
     pantalla.blit(vb,(5,10))
+    vb=pygame.image.load('grapy/img/ab.png').convert_alpha()    
+    pantalla.blit(vb,(5,60))
     
 
 
-def main(pantalla,v):
+def Principal(pantalla,v):
         #Captura de teclas
-        Lienzo(v)
+        v=Lienzo(pantalla,v)
         pantalla.fill(BLANCO)
         seleccion(pantalla)
         v.update(pantalla)
+        #print len(v)
+        return v
         
-def Lienzo(lista):
+def Lienzo(pantalla, lista):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x,y=event.pos
-                for cuadro in lista:
-                        if cuadro.rect.collidepoint(event.pos):
-                                cuadro.click = True
-                        elif x<=55 and y<=55:
-                                print 'nuevo'                    
+                for ve in lista:
+                        if ve.rect.collidepoint(event.pos):
+                                ve.click = True
+                                ve.sel=True
+                                #ve.rect.center = pygame.mouse.get_pos()
+                                
+                if x<=55 and y<=55:
+                     # Nuevo vertice
+                     v = Vertice()
+                     #v.rect.center = pantalla.get_rect().center
+                     v.rect.center = (55,50)
+                     v.id=len(lista)
+                     lista.add(v)
+                     col=True
+                     while col:
+                           col=False
+                           colision=pygame.sprite.spritecollide(v, lista, False)
+                           for e in colision:
+                               if v.id != e.id:
+                                  v.rect.left = e.rect.right
+                                  col=True
+                     
+                     #lista.add(v)
+                     
+                     
+                
             elif event.type == pygame.MOUSEBUTTONUP:
-                for cuadro in lista:
-                        cuadro.click = False
+                for ve in lista:
+                    if ve.sel==True:
+                        colision=pygame.sprite.spritecollide(ve, lista, False)
+                        for e in colision:
+                            if ve != e:
+                               ve.rect.left = e.rect.right
+                               if ve.rect.x >= ANCHO:
+                                  ve.rect.right = e.rect.left
+                        ve.click=False
+                        ve.sel=False
             elif event.type == pygame.QUIT:
                 pygame.quit(); sys.exit()
+                
+        #colision=pygame.sprite.spritecollide(player, block_list, True)
+        return lista
 
 
 def Pantalla():
         os.environ['SDL_VIDEO_CENTERED'] = '1'
         pygame.init()
-        pantalla = pygame.display.set_mode((1000,600))
+        pantalla = pygame.display.set_mode((ANCHO, ALTO))
         reloj = pygame.time.Clock()
         vertices=pygame.sprite.Group()
+        '''
         v = Vertice()
         v.rect.center = pantalla.get_rect().center
         v.sel=True
         vertices.add(v)
+        '''
         while 1:
-            main(pantalla,vertices)
+            vertices=Principal(pantalla,vertices)
             pygame.display.update()
